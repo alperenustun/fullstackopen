a13c5ec38c2c20d5638bfd397ea08cf9cccac44c
Alperen Üstün <alperen.ustun39@gmail.com>
2023-12-10 16:54:53 +0300

    [1.3.-1.5.] Completed

---
+++ part1/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises =
+    parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
